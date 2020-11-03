50147ec64811fecef3665a4c9fda38ba0d4db0fc
caloloc2 <calolomino@gmail.com>
2020-11-03 13:19:17 -0500

    actualiza

--- deber2.py
+++ deber2.py
@@ -3,16 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 prob = [0.5, 0.5] # probabilidad del 50% de moverse a la izquierda o derecha
-n = 2 # numero de pasos
+n = 5 # absorbing barrier
 
-inicio = 0 # inicio establecido en cero
-positions = [inicio]
+pos_actual = 0 # posicion inicial establecida en cero
+pos_contador = 0 # contador de pasos
+iteraciones = True # bandera para iteraciones
 
-rr = np.random.random(n)
-print(rr)
-izqp = rr < prob[0]
-derp = rr < prob[1]
+while(iteraciones): # itera mientras la variable sea verdadera
+    rr = np.random.random() # obtiene un valor randomico
+    if (rr > prob[0]): # derecha (dependiendo de la probabilidad asignada) 
+        pos_actual += 1
+    else: # izquierda
+        pos_actual -= 1
+    
+    pos_contador += 1
 
-print(izqp)
-# for i_izq, i_der in zip(izqp, derp):
-#     izquierda = i_izq and positions[-1] > 1
+    if (pos_actual==n): # si se encuentra con absorbing barrier        
+        iteraciones = False # termina las iteraciones
+
+print("Se requirieron "+str(pos_contador)+" pasos hasta la absorning barrier en n="+str(n))
